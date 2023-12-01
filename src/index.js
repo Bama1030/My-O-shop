c75fc0e0144537b18004a3b4eff2dadbfc56abe7
Bapi Mandal <bapimandal12340@gmail.com>
2023-12-01 18:25:05 +0530

    second

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../frontend/src/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import Store from "./redux/store";
